c556c18db606f6a91c1b2dcdc040e6ef9db792c0
Joshua Calvert <46970489+JSC0x62@users.noreply.github.com>
2021-10-12 13:25:57 -0500

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -1,7 +1,14 @@
+/*
+https://leetcode.com/problems/two-sum/
+(my solution, very simillar to the defualt brute-force answer)
+*/
 class Solution {
     public int[] twoSumMySolution(int[] nums, int target) {
+       //Start at the first element (i)
         for(int i = 0; i < nums.length; i++)
         {
+            //from here, compare the next element (j) with i to see if they = target
+            //if not, i is increased by 1 and we try again
             for(int j = i + 1; j < nums.length; j++){
                 if(nums[i] + nums[j] == target){
                     System.out.println(nums[i] + nums[j]);
