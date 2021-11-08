41e8b8d3faa17436db18565e07d929c6c9c4755b
Joshua Calvert <46970489+JSC0x62@users.noreply.github.com>
2021-11-08 16:52:39 -0600

    Update Product of array except self (medium).java

--- Product of array except self (medium).java
+++ Product of array except self (medium).java
@@ -6,6 +6,46 @@ O(n) solution. Passes through twice but nothing is nested so should still be n.
 The general idea is that for each element we have to multiply everything except itself.
 So you have a prefix and a postfix for each number.
 
+Example:
+
+nums = [1,2,3,4]
+
+prefix loop:
+
+1) i = 0, output[0] = prefix (1), 
+prefix = nums[0] (1) * prefix (1), (prefix = 1)
+[1,,,] //tecnically this happens on the first line but im just showing the result after each time through the loop
+
+2) i = 1, output[1] = prefix (1)
+prefix = nums[1] (2) * prefix (1), (prefix = 2)
+[1,1,,]
+
+3) i = 2, output[2] = prefix (2)
+prefix = nums[2] (3) * prefix (2), (prefix = 6)
+[1,1,2,]
+
+4) i = 3, output[3] = prefix (6)
+prefix = nums[3] (4) * prefix (6), (prefix = 24) //this doesnt matter
+[1,1,2,6]
+
+Postfix loop:
+
+1) j = 3, output[3] = output[3] (6) * postfix (1) = 6, (output[3] = 6)
+postfix = postfix (1) * nums[4] = 4, (postfix = 4)
+[1,1,2,6]
+
+2) j = 2, output[2] = output[2] (2) * postfix (4) = 8, (output[2] = 8)
+postfix = postfix (4) * nums[2] (3) * postfix (4) = 12, (postfix = 12)
+[1,1,8,6]
+
+3) j = 1, output[1] = output[1] (1) * postfix (12) = 12, (output[1] = 12)
+postfix = postfix (12) * nums[1] (2) * postfix (12) = 24, (postfix = 24)
+[1,12,8,6]
+
+4) j = 0, output[0] (1) * postfix (24) = 24, (output[0] = 24)
+//we are done now..
+Final result (this is returned) [24,12,8,6]
+
 */
 
 class Solution {
