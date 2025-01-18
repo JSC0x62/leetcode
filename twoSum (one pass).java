b982176db750da3c399010760cb34a18a943252d
Joshua Calvert <46970489+JSC0x62@users.noreply.github.com>
2025-01-18 14:24:05 -0600

    Update twoSum (one pass).java
    updating with better comments and 1 line return of solution

--- twoSum (one pass).java
+++ twoSum (one pass).java
@@ -1,26 +1,26 @@
 import java.util.HashMap;
 /*
 This solution uses the One Pass algorithim and is much faster at O(n) than brute force at O(n^2)
+
 */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //0) use hashmap to store k = element value  v = index
         HashMap<Integer, Integer> map = new HashMap<>();
-        //one pass implementation
+        
+        //1) one pass implementation, iterate through nums[]
         for(int i = 0; i < nums.length; i++){
-            //for each num in arr
-            //we are looking for x: target - i = x
-            //check if x is in the map
+            //2) calculate the missing element to reach target
             int x = target - nums[i];
+            
+            //3) check if we have visited x
             if(map.containsKey(x)){
-              //if x is here, we are done. Return it and i
-                int[] result = new int[]{i, map.get(x)};
-                return result;
-            }
-            else{
-              //else we add this to our map to keep track of
-                map.put(nums[i], i);
+            //4) if we have visited x then we can return the indices values of num and x
+                return new int[] {map.get(x), i};
             }
             
+              //if not add the current value and its index to the map
+                map.put(nums[i], i);
         }
         return nums;
     }
