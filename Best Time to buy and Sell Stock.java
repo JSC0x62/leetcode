d5ffe197f35a11acb2b81415d757f5a1c13f5a1b
Joshua Calvert <46970489+JSC0x62@users.noreply.github.com>
2025-01-19 12:05:40 -0600

    Update Best Time to buy and Sell Stock.java
    Updated version

--- Best Time to buy and Sell Stock.java
+++ Best Time to buy and Sell Stock.java
@@ -24,33 +24,33 @@ public class Solution {
     
     /*
     here is an optimal solution O(N). Using 2 pointers and sliding window tecnique.
+    updated to be more readable
     */
     
      public int maxProfit(int[] prices) {
-        int buy = 0;
-        int sell = 1;
+
+        //1) use a left (buy) pointer and right (sell) pointer. l = 0 r = 1
+        
+        int l = 0;
+        int r = 1;
+        int profit = 0;
         int maxProfit = 0;
-        int currentProfit = 0;
-        int length = prices.length;
-       while(sell < length){  
-           //2 pointers, 1 for current buy price, 1 for current sell price (set to 1 day after the buy price)
-           //first, check if the current buy and sell prices is a profitable transaction
-           if(prices[sell] > prices[buy]){
-               //if so, update the current profit. 
-               currentProfit = prices[sell] - prices[buy];
-               //if its > max profit, update it
-               if(currentProfit > maxProfit){
-                   maxProfit = currentProfit;
-               }
-           }
-           else{
-               //if its not profitable we need to move the buy date up 1.
-               buy = sell;
-           }
-           //the sell date is always incremented. We keep going until the end.
-           //The buy date is only incremented if the transaction isn't profitable. 
-           sell++;
-       }
-        return maxProfit;
+
+        while(r < prices.length){
+           //2) if l < r (buy < sell) - calculate profit
+            if(prices[l] < prices[r]){
+                profit = prices[r] - prices[l];
+                if(profit > maxProfit){
+                    maxProfit = profit;
+                }
+            }
+            else{
+               //3) else move left pointer to where right is
+                l = r;   
+            }
+           //4) shift right pointer (always will shift by 1 to iterate through the array)
+            r++;
+        }
+        return maxProfit;       
     }
 }
